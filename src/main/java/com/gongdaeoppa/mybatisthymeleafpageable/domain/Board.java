3160b6d1edbd198f93ac53026deb86fe98ac5b4b
DESKTOP-5Q440Q3\seung <hayeseung@gmail.com>
2022-07-27 11:28:53 +0900

    마이바티스(mybatis) / 타임리프(thymeleaf) / 페이징 처리

--- src/main/java/com/gongdaeoppa/mybatisthymeleafpageable/domain/Board.java
+++ src/main/java/com/gongdaeoppa/mybatisthymeleafpageable/domain/Board.java
@@ -1,9 +1,6 @@
 package com.gongdaeoppa.mybatisthymeleafpageable.domain;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @Setter
@@ -14,5 +11,10 @@ public class Board {
     private Long id;
     private String title;
     private String regDay;
+
+
+    public String getRegDayForPrint() {
+        return regDay.substring(2, 16);
+    }
 
 }
